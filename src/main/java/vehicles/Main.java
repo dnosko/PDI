f0ead0c8c85843b2598ca8bb597a4d495f3aa6c4
Daša Nosková <dasa.noskova@gmail.com>
2023-12-19 16:41:53 +0100

    moved from one class to multiple

--- src/main/java/vehicles/Main.java
+++ src/main/java/vehicles/Main.java
@@ -6,9 +6,6 @@ import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.*;
 import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringEncoder;
-import org.apache.flink.api.common.state.ListState;
-import org.apache.flink.api.common.state.ListStateDescriptor;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.api.java.utils.ParameterTool;
@@ -20,33 +17,17 @@ import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.sink.filesystem.rollingpolicies.DefaultRollingPolicy;
-import org.apache.flink.streaming.api.functions.source.RichSourceFunction;
-
 import org.apache.flink.streaming.api.functions.windowing.ProcessAllWindowFunction;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows;
 import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.api.windowing.windows.GlobalWindow;
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
-
 import org.apache.flink.util.Collector;
-import org.asynchttpclient.AsyncHttpClient;
-import org.asynchttpclient.BoundRequestBuilder;
-import org.asynchttpclient.Dsl;
-import org.asynchttpclient.ws.WebSocket;
-import org.asynchttpclient.ws.WebSocketListener;
-import org.asynchttpclient.ws.WebSocketUpgradeHandler;
 import lombok.extern.slf4j.Slf4j;
-import javax.xml.bind.DatatypeConverter;
-import java.io.IOException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.time.Duration;
 import java.util.*;
-import java.util.concurrent.ArrayBlockingQueue;
-import java.util.concurrent.BlockingQueue;
+
 
 /**
  * TODO:
@@ -72,7 +53,7 @@ public class Main {
         ParameterTool parameters = ParameterTool.fromArgs(args);
         env.getConfig().setGlobalJobParameters(parameters);
 
-        DataStreamSource<String> mySocketStream = env.addSource(new MyWebSocketSourceFunc());
+        DataStreamSource<String> mySocketStream = env.addSource(new WebSocketStream());
         DataStream<JsonNode> jsonStream = mySocketStream.map(jsonString -> mapToJson(jsonString));
         DataStream<Vehicle> vehicleStream = jsonStream.map(s -> mapToVehicle(s)).filter(new IsActiveFilter());
         env.enableCheckpointing(CHECKPOINTING_INTERVAL_MS);
@@ -132,7 +113,7 @@ public class Main {
         /**************************************************************************************/
         //env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime);
 
-        /* top 5 delayed vehicles of all time, sorting doesnt work, skontrolovat ci sa nepridavaju tie iste dokola... */
+        /* top 5 delayed vehicles of all time, sorting doesnt work...*/
         DataStream<Vehicle> delayedStream = vehicleStream.filter(v -> v.delay > 0.0).assignTimestampsAndWatermarks(WatermarkStrategy
                 .<Vehicle>forMonotonousTimestamps()
                 .withTimestampAssigner((event, timestamp) -> event.getLastUpdateLong()));
@@ -171,63 +152,6 @@ public class Main {
 
         env.execute(JOB_NAME);
 
-    }
-
-    /* mozno zmenit na GLobalWindow ak nebude funvoat a fungovat na tom.*/
-    public static class MostDelayedProcessAllWindowFunction extends ProcessAllWindowFunction<Vehicle, Vehicle, TimeWindow> {
-        private transient ListState<Vehicle> mostDelayedVehicles;
-        private final int topN = 5;
-
-        @Override
-        public void open(Configuration parameters) {
-            ListStateDescriptor<Vehicle> descriptor =
-                    new ListStateDescriptor<>("mostDelayedState", TypeInformation.of(Vehicle.class));
-            mostDelayedVehicles = getRuntimeContext().getListState(descriptor);
-        }
-
-        @Override
-        public void process(Context context, Iterable<Vehicle> elements, Collector<Vehicle> out) throws Exception {
-            // add vehicles to new list for sorting
-            Iterable<Vehicle> current = mostDelayedVehicles.get();
-            List<Vehicle> currentList = new ArrayList<>();
-
-            for (Vehicle v : current) {
-                currentList.add(v);
-            }
-
-            // Update state with the new vehicles
-            for (Vehicle vehicle : elements) {
-                // test if vehicle with same id is already in list
-                Vehicle testIfAlreadyInList = vehicleAlreadyInList(mostDelayedVehicles.get(), vehicle);
-                if (testIfAlreadyInList != null) {
-                    // remove old vehicle instance so it can be replaced by the most recent record
-                    currentList.remove(testIfAlreadyInList);
-                }
-                // add new vehicle to list
-                currentList.add(vehicle);
-
-            }
-
-            currentList.sort(Comparator.comparing(Vehicle::getDelay).reversed());
-
-            mostDelayedVehicles.clear();
-            // keep only topN delayed vehicles
-            mostDelayedVehicles.addAll(currentList.subList(0, Math.min(topN, currentList.size())));
-
-            // Emit the two most delayed vehicles
-            for (Vehicle vehicle : mostDelayedVehicles.get()) {
-                out.collect(vehicle);
-            }
-        }
-
-        private Vehicle vehicleAlreadyInList(Iterable<Vehicle> elements, Vehicle v) {
-            for (Vehicle alredyIn : elements) {
-                if (v.getId().equals(alredyIn.getId())) {
-                    return alredyIn;
-                }
-            }
-            return null;
-        }
     }
 
 
@@ -298,110 +222,6 @@ public class Main {
         }
 
 
-    // from https://gist.github.com/tonvanbart/17dc93be413f7c53b76567e10b87a141
-    public static class MyWebSocketSourceFunc extends RichSourceFunction<String> {
-        private boolean running = true;
-        transient AsyncHttpClient client;
-        transient BoundRequestBuilder boundRequestBuilder;
-        transient WebSocketUpgradeHandler.Builder webSocketListener;
-        private BlockingQueue<String> messages = new ArrayBlockingQueue<>(100);
 
-        private final String websocketURI = "wss://gis.brno.cz/geoevent/ws/services/ODAE_public_transit_stream/StreamServer/subscribe?outSR=4326";
-
-        @Override
-        public void run(SourceContext<String> ctx) throws Exception {
-            WebSocketUpgradeHandler webSocketUpgradeHandler = webSocketListener.addWebSocketListener(
-                    new WebSocketListener() {
-
-                        private final ObjectMapper myMapper = new ObjectMapper();
-
-                        private String getRsvpId(String payload) {
-                            try {
-                                Map map = myMapper.readValue(payload, Map.class);
-                                Object rsvpId = map.get("rsvp_id");
-                                return rsvpId != null ? rsvpId.toString() : "NOT FOUND";
-                            } catch (IOException e) {
-                                log.error("Mapping failed, returning 'null'");
-                                return "NULL";
-                            }
-                        }
-
-                        @Override
-                        public void onOpen(WebSocket webSocket) {
-                        }
-
-                        @Override
-                        public void onClose(WebSocket webSocket, int i, String s) {
-                        }
-
-                        @Override
-                        public void onError(Throwable throwable) {
-                        }
-
-                        @Override
-                        public void onTextFrame(String payload, boolean finalFragment, int rsv) {
-                            log.debug("onTextFrame({}), rsvp_id={}", hash(payload), getRsvpId(payload));
-                            //System.out.println(payload);
-                            if (payload != null) {
-                                try {
-                                    messages.put(payload);
-                                } catch (InterruptedException e) {
-                                    log.error("Interrupted!", e);
-                                    Thread.currentThread().interrupt();
-                                }
-                            }
-                        }
-                    }).build();
-            boundRequestBuilder.execute(webSocketUpgradeHandler).get();
-
-            while (running) {
-                ctx.collect(messages.take());
-            }
-            running = false;
-        }
-
-        @Override
-        public void cancel() {
-            log.info("cancel function called");
-            running = false;
-        }
-
-        @Override
-        public void open(Configuration parameters) throws Exception {
-            log.info("open function called");
-            super.open(parameters);
-            client = Dsl.asyncHttpClient();
-            boundRequestBuilder = client.prepareGet(websocketURI);
-            webSocketListener = new WebSocketUpgradeHandler.Builder();
-        }
-
-        private String hash(String input) {
-            if (input == null) {
-                return "-- NULL --";
-            }
-
-            try {
-                MessageDigest md = MessageDigest.getInstance("MD5");
-                md.update(input.getBytes());
-                byte[] digest = md.digest();
-                return DatatypeConverter.printHexBinary(digest).toUpperCase();
-            } catch (NoSuchAlgorithmException e) {
-                log.error("Cound not instantiate MD5", e);
-                return "--NOT CALCULATED--";
-            }
-        }
-    }
-
-    public static class MapIt extends RichMapFunction<String, String> {
-
-        final ObjectMapper objectMapper = new ObjectMapper();
-
-        @Override
-        public String map(String value) throws Exception {
-            Map<String, Object> mapped = objectMapper.readValue(value, Map.class);
-            Object rsvp = mapped.get("rsvp_id");
-            return rsvp != null ? rsvp.toString() : "null" ;
-        }
-    }
 }
 
