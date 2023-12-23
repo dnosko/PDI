853919d510a7931e8b80a730c65d45e0d68e62d6
Daša Nosková <dasa.noskova@gmail.com>
2023-12-23 19:53:28 +0100

    added license

--- src/main/java/vehicles/WebSocketStream.java
+++ src/main/java/vehicles/WebSocketStream.java
@@ -1,3 +1,29 @@
+/* Adapted from:  https://github.com/tonvanbart/flink-examples/blob/master/websocket-sourcefunction/src/main/java/org/vanbart/functions/WebSocketSourceFunction.java
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+        OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+        ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+*/
 package vehicles;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -18,7 +44,6 @@ import java.util.concurrent.BlockingQueue;
 import javax.xml.bind.DatatypeConverter;
 import lombok.extern.slf4j.Slf4j;
 
-// from https://github.com/tonvanbart/flink-examples/blob/master/websocket-sourcefunction/src/main/java/org/vanbart/functions/WebSocketSourceFunction.java
 @Slf4j
 public class WebSocketStream extends RichSourceFunction<String> {
     private boolean running = true;
